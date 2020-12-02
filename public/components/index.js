82af904dd793244bdff303b0e0ec3c4df15b6946
dem-14 <60597957+dem-14@users.noreply.github.com>
2020-12-02 19:19:59 +0100

    mejoras en el codigo

--- public/components/index.js
+++ public/components/index.js
@@ -9,12 +9,9 @@ class Index extends HTMLElement {
         <ul>
           <lh>Índice</lh>
       <a href="../index.html"><li>Sobre Mi</li></a>
-      <a href="../pages/resume.html"><li>Formación </li></a>
-      <a href="../pages/resume.html"><li>Experiencia laboral</li></a>
-  <a href="../pages/skills.html"><li>Tecnologias</li></a>
-  <a href="../pages/skills.html"><li>Idiomas</li></a>
-  <a href="../pages/skills.html"><li>Competencias</li></a>
-  <a href="../pages/skills.html"><li>Otros datos de interes</li></a>
+      <a href="../pages/resume.html"><li>Formación y Experiencia laboral  </li></a>
+      
+  <a href="../pages/skills.html"><li>Habilidades</li></a>
   </ul>
         `;
       }
